1d2adc6fb3bc08c57b8d40850031609de95676fe
minurajasinghe <157316121+minurajasinghe@users.noreply.github.com>
2025-09-21 18:30:47 -0400

    Update test_function_01.m

--- test_function_01.m
+++ test_function_01.m
@@ -6,37 +6,17 @@ function [f_val, J] = test_function_01(X)
     f3 = sin(X(1) + X(2) + X(3));
     f_val = [f1, f2, f3];
 
-    df1_x = 2*X(1) 
-    df1_y = 2*X(2)
-    df1_z = 5*X(3)^4
+    df1_x = 2*X(1); 
+    df1_y = 2*X(2);
+    df1_z = 5*X(3)^4;
 
-    df2_x = X(3)
-    df2_y = 1
-    df2_z = X(1)
+    df2_x = X(3);
+    df2_y = 1;
+    df2_z = X(1);
 
     df3_x = cos(X(1) + X(2) + X(3)); 
     df3_y = cos(X(1) + X(2) + X(3)); 
     df3_z = cos(X(1) + X(2) + X(3));
 
-    J = [df1_x df1_y df1_z, df2_x df2_y df2_z, df3_x, df3_y, df3_z];
+    J = [df1_x, df1_y, df1_z; df2_x, df2_y, df2_z; df3_x, df3_y, df3_z];
 end
-
-function jacobian = approx_Jacobian(func, n, m)
-    df1_x = 2*X(1) 
-    df1_y = 2*X(2)
-    df1_z = 5*X(3)^4
-
-    df2_x = X(3)
-    df2_y = 1
-    df2_z = X(1)
-
-    df3_x = cos(X(1) + X(2) + X(3)); 
-    df3_y = cos(X(1) + X(2) + X(3)); 
-    df3_z = cos(X(1) + X(2) + X(3)); 
-
-
-    J = [df1]
-    
-end
-
-%A way to solve n equations with N variables
