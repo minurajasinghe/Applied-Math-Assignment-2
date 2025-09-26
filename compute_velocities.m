728d57ca99203086140ed27dc35dd81a7d4a6882
Addison Fisher <afisher@olin.edu>
2025-09-26 14:57:31 -0400

    adding final functions

---
+++ compute_velocities.m
@@ -0,0 +1,11 @@
+%Computes the theta derivatives of each vertex coordinate for the Jansen linkage
+%INPUTS:
+%vertex_coords: a column vector containing the (x,y) coordinates of every vertex
+% these are assumed to be legal values that are roots of the error funcs!
+%leg_params: a struct containing the parameters that describe the linkage
+%theta: the current angle of the crank
+%OUTPUTS:
+%dVdtheta: a column vector containing the theta derivates of each vertex coord
+function dVdtheta = compute_velocities(vertex_coords, leg_params, theta)
+    %your code here
+end
